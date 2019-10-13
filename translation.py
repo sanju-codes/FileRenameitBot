24b2160cdd3fc46113b343c1ad5dc32f4d30cda4
SANJU PAPPACHAN <sanjupappachan8943@gmail.com>
2019-10-13 11:08:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
-    START_TEXT = """This is an @AnyDLBot clone created using the Open Source code available.
-Source Code: https://github.com/SpEcHiDe/AnyDLBot
-© @AnyDLBot"""
+    START_TEXT = """This is an @FileRenameitBot clone created using the Open Source code available.
+Source Code: https://github.com/Sanju8943/RenameBot
+© @FileRenameitBot"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "This is an @AnyDLBot clone created using the Open Source <a href='https://github.com/SpEcHiDe/AnyDLBot'>code</a>. \nStart @AnyDLBot to know Paid Plans!"
